adc761ebab4d6e0d8cbe8cdee43cc0eb9298bd00
tony <yahooleo36@gmail.com>
2026-06-02 23:42:03 +0800

    test

--- apps/cms/src/use-page-price.ts
+++ apps/cms/src/use-page-price.ts
@@ -1,5 +1,5 @@
 import { formatPrice } from "@demo/shared";
-import { minus } from "@demo/utils";
+import { minus } from "@demo/utils/minus";
 
 export function usePagePrice(price: number) {
   minus(5, 2);
