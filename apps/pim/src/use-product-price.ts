c93322c2f28c839aab7d7331ea52936d047d8bc0
tony <yahooleo36@gmail.com>
2026-06-02 23:33:16 +0800

    test

--- apps/pim/src/use-product-price.ts
+++ apps/pim/src/use-product-price.ts
@@ -1,6 +1,8 @@
 import { formatPrice } from "@demo/shared";
+import { minus } from "@demo/utils";
 
 export function useProductPrice(price: number) {
+  minus(price, 10);
   return {
     display: formatPrice(price),
     raw: price,
